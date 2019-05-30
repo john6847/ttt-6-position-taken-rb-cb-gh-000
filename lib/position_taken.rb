676e08751c90474ae6805a23d792507e4d6dd04e
John Bien-aime <20131256@ce.pucmm.edu.do>
2019-05-30 03:26:21 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  if board[index].empty?
+  if board[index].empty? || board[index] == " " || board[index] == "" || index.nil?
     return false
   end
   return true
