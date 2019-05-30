2139468372d3b17c87ce3ace38fcdbfd2e4f6340
John Bien-aime <20131256@ce.pucmm.edu.do>
2019-05-30 03:22:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
-def position_taken(index_number)
-  if array[index_number].empty?
+def position_taken(board,index_number)
+  if board[index_number].empty?
     return false
   end
   return true
